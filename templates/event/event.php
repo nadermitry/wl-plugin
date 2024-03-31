69f1ce7a99f7639ad36453c86b92c69f725449ad
nadermitry@gmail.com <nadermitry@gmail.com>
2024-03-31 06:35:33 +0200

    yyyy

--- templates/event/event.php
+++ templates/event/event.php
@@ -57,7 +57,81 @@
 
 
 
-        
+
+        .modal.show .modal-dialog {
+    transform: none;
+}
+.modal.fade .modal-dialog {
+    transition: transform 0.3s ease-out;
+    transform: translate(0, -50px);
+}
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1060;
+    display: none;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    outline: 0;
+}
+
+.modal-content {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    pointer-events: auto;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 0.3rem;
+    outline: 0;
+}
+
+.modal-dialog {
+    position: relative;
+    width: auto;
+    margin: 0.5rem;
+    pointer-events: none;
+}
+
+.modal-header {
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 1rem;
+    border-bottom: 1px solid #dee2e6;
+    border-top-left-radius: calc(0.3rem - 1px);
+    border-top-right-radius: calc(0.3rem - 1px);
+}
+
+.modal-title {
+    margin-bottom: 0;
+    line-height: 1.5;
+}
+.modal-body {
+    position: relative;
+    flex: 1 1 auto;
+    padding: 1rem;
+}
+
+.modal-footer {
+    display: flex;
+    flex-wrap: wrap;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0.75rem;
+    border-top: 1px solid #dee2e6;
+    border-bottom-right-radius: calc(0.3rem - 1px);
+    border-bottom-left-radius: calc(0.3rem - 1px);
+}
+
+
 
 </style>
 
@@ -358,8 +432,7 @@ add Gifts
       <div class="modal-body">     
 
         <!-- Modal Content Goes Here -->
-        <div id="primary" class="content-area">
-          <main id="main" class="site-main">
+        
             <div class="container">
                <div class="row">
                   <div class="col-md-12">                    
@@ -417,8 +490,7 @@ add Gifts
                 </div>
             </div>
         </div>
-    </main><!-- #main -->
-</div><!-- #primary -->
+   
         
       </div>
 
