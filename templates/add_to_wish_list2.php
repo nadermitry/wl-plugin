e0bd1b69a4768f22e359303e969860ebc0b3fdef
Nader Mitry <johndoe@example.com>
2024-04-29 11:29:38 +0200

    ss
    Signed-off-by: Nader Mitry <johndoe@example.com>

--- templates/add_to_wish_list2.php
+++ templates/add_to_wish_list2.php
@@ -1,6 +1,130 @@
+<style>
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgba(0,0,0,0.5); /* Black w/ opacity */
+  z-index: 999;
+}
+
+.modal.show .modal-dialog {
+    transform: none;
+}
+.modal.fade .modal-dialog {
+    transition: transform 0.3s ease-out;
+    transform: translate(0, -50px);
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 10% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 30%;
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+/* Loading Spinner */
+.wl-loader {
+  border: 8px solid #f3f3f3; /* Light grey */
+  border-top: 8px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -25px;
+  margin-top: -25px;
+  
+}
+
+.fade:not(.show) {
+    opacity: 100;
+}
 
 
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 
+
+<style>
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgba(0,0,0,0.5); /* Black w/ opacity */
+  z-index: 999;
+}
+
+.modal.show .modal-dialog {
+    transform: none;
+}
+.modal.fade .modal-dialog {
+    transition: transform 0.3s ease-out;
+    transform: translate(0, -50px);
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 10% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 30%;
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+/* Loading Spinner */
+.wl-loader {
+  border: 8px solid #f3f3f3; /* Light grey */
+  border-top: 8px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -25px;
+  margin-top: -25px;
+  
+}
+
+.fade:not(.show) {
+    opacity: 100;
+}
+
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
 <?php
   global $product;
   $product_id = $product->get_id();
@@ -68,7 +192,25 @@
 
 
 
-
+<!-- Modal -->
+<div class="modal fade" id="addRemovefromWishlistMessageModal" tabindex="-1" aria-labelledby="addRemovefromWishlistMessageModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addRemovefromWishlistMessageModalLabel">Modal Title</h5>
+        
+          <span id="x" class="close" aria-hidden="true">&times;</span>
+      
+      </div>
+      <div class="modal-body">
+      <p id="modalMessage"></p>
+      </div>
+      <div class="modal-footer">
+        <button id="myBtn" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
@@ -246,12 +388,12 @@ function remove_from_gifts(record_id){
 
   
   function showLoading() {	
-	var modal = document.getElementById("loadingModal");
+	var modal = document.getElementById("addRemovefromWishlistMessageModal");
 	modal.style.display = "block";
   }
 
   function hideLoading() {	
-	var modal = document.getElementById("loadingModal");
+	var modal = document.getElementById("addRemovefromWishlistMessageModal");
 	modal.style.display = "none";
   }
 
