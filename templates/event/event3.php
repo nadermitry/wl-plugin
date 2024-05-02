323dd77ce1d65da38ff77ee54af168c630387eb6
nadermitry@gmail.com <nadermitry@gmail.com>
2024-05-02 11:38:51 +0300

    hh

--- templates/event/event3.php
+++ templates/event/event3.php
@@ -1,3 +1,227 @@
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Nunito:400,600,700");
+* {
+  box-sizing: border-box;
+}
+
+
+
+.modal1 {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  background: rgba(51, 51, 51, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition: 0.4s;
+}
+.modal-container {
+  display: flex;
+  max-width: 720px;
+  width: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+  position: absolute;
+  opacity: 0;
+  pointer-events: none;
+  transition-duration: 0.3s;
+  background: #fff;
+  transform: translateY(100px) scale(0.4);
+}
+.modal-title {
+  font-size: 26px;
+  margin: 0;
+  font-weight: 400;
+  color: #55311c;
+}
+.modal-desc {
+  margin: 6px 0 30px 0;
+}
+.modal-left {
+  padding: 60px 30px 20px;
+  background: #fff;
+  flex: 1.5;
+  transition-duration: 0.5s;
+  transform: translateY(80px);
+  opacity: 0;
+}
+.modal-button {
+  color: #7d695e;
+  font-family: "Nunito", sans-serif;
+  font-size: 18px;
+  cursor: pointer;
+  border: 0;
+  outline: 0;
+  padding: 10px 40px;
+  border-radius: 30px;
+  background: white;
+  box-shadow: 0 10px 40px rgba(0, 0, 0, 0.16);
+  transition: 0.3s;
+}
+.modal-button:hover {
+  border-color: rgba(255, 255, 255, 0.2);
+  background: rgba(255, 255, 255, 0.8);
+}
+.modal-right {
+  flex: 2;
+  font-size: 0;
+  transition: 0.3s;
+  overflow: hidden;
+}
+.modal-right img {
+  width: 100%;
+  height: 100%;
+  transform: scale(2);
+  -o-object-fit: cover;
+     object-fit: cover;
+  transition-duration: 1.2s;
+}
+.modal1.is-open {
+  height: 100%;
+  background: rgba(51, 51, 51, 0.85);
+}
+.modal1.is-open .modal-button {
+  opacity: 0;
+}
+.modal1.is-open .modal-container {
+  opacity: 1;
+  transition-duration: 0.6s;
+  pointer-events: auto;
+  transform: translateY(0) scale(1);
+}
+.modal1.is-open .modal-right img {
+  transform: scale(1);
+}
+.modal1.is-open .modal-left {
+  transform: translateY(0);
+  opacity: 1;
+  transition-delay: 0.1s;
+}
+.modal-buttons {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.modal-buttons a {
+  color: rgba(51, 51, 51, 0.6);
+  font-size: 14px;
+}
+
+.sign-up {
+  margin: 60px 0 0;
+  font-size: 14px;
+  text-align: center;
+}
+.sign-up a {
+  color: #8c7569;
+}
+
+.input-button {
+  padding: 8px 12px;
+  outline: none;
+  border: 0;
+  color: #fff;
+  border-radius: 4px;
+  background: #8c7569;
+  font-family: "Nunito", sans-serif;
+  transition: 0.3s;
+  cursor: pointer;
+}
+.input-button:hover {
+  background: #55311c;
+}
+
+.input-label {
+  font-size: 11px;
+  text-transform: uppercase;
+  font-family: "Nunito", sans-serif;
+  font-weight: 600;
+  letter-spacing: 0.7px;
+  color: #8c7569;
+  transition: 0.3s;
+}
+
+.input-block {
+  display: flex;
+  flex-direction: column;
+  padding: 10px 10px 8px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  transition: 0.3s;
+}
+.input-block input {
+  outline: 0;
+  border: 0;
+  padding: 4px 0 0;
+  font-size: 14px;
+  font-family: "Nunito", sans-serif;
+}
+.input-block input::-moz-placeholder {
+  color: #ccc;
+  opacity: 1;
+}
+.input-block input:-ms-input-placeholder {
+  color: #ccc;
+  opacity: 1;
+}
+.input-block input::placeholder {
+  color: #ccc;
+  opacity: 1;
+}
+.input-block:focus-within {
+  border-color: #8c7569;
+}
+.input-block:focus-within .input-label {
+  color: rgba(140, 117, 105, 0.8);
+}
+
+.icon-button {
+  outline: 0;
+  position: absolute;
+  right: 10px;
+  top: 12px;
+  width: 32px;
+  height: 32px;
+  border: 0;
+  background: 0;
+  padding: 0;
+  cursor: pointer;
+}
+
+.scroll-down {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  color: #7d695e;
+  font-size: 32px;
+  font-weight: 800;
+  transform: translate(-50%, -50%);
+}
+.scroll-down svg {
+  margin-top: 16px;
+  width: 52px;
+  fill: currentColor;
+}
+
+@media (max-width: 750px) {
+  .modal-container {
+    width: 90%;
+  }
+
+  .modal-right {
+    display: none;
+  }
+}
+</style>
 
 
 <?php 
@@ -51,19 +275,24 @@ $end_time = $endDateTime->format('h:i A'); // Time in 'HH:MM:SS' format
 <div class="col-md-5">
   <!-- Product Description -->
   <div class="product-description">
+  <h1> <?php echo stripcslashes(sanitize_text_field($result->title)); ?>
+  <?php if ($isCurrentUser) :?>
+    <button data-toggle="modal" data-target="#editModal" type="button" class="nam-edit-button">
+        <i class="fas fa-edit"></i> Edit
+      </button>
+      <?php endif ?>
+    </h1>
    <!-- <span>Event</span>-->
-   <?php if ($isCurrentUser) :?>
+   
              
     
 
-             <div class="product-price">
+    <div class="product-price">
     <!--<span>148$</span>-->
-    <a   data-toggle="modal" data-target="#editModal" href="#" class="cart-btn">Edit</a>
-    
     </div>
  
-         <?php endif ?>
-    <h1> <?php echo stripcslashes(sanitize_text_field($result->title)); ?></h1>
+       
+   
     <p><?php echo stripcslashes(sanitize_text_field($result->description)) ; ?></p>
   </div>
 
@@ -474,6 +703,109 @@ $end_time = $endDateTime->format('h:i A'); // Time in 'HH:MM:SS' format
 
 
 
+  <div class="modal1">
+  
+  <div class="modal-container">
+  
+    <div class="modal-left">
+    <form id="event-form" method="post" enctype="multipart/form-data">
+      
+      <div class="container">
+     
+        <div class="input-block">
+          <label for="event_title" class="input-label">Title:</label>
+          <input type="text" value=" <?php echo stripcslashes(sanitize_text_field($result->title)); ?>" id="event_title" name="event_title" required>
+        </div>   
+     
+          <div class="row">
+            <div class="col-md-6">
+              <div class="input-block">
+              <label  for="start_datetime" class="input-label" >Start Date and Time:</label>
+                <input type="datetime-local"  value="<?php echo $result->start_date; ?>" id="start_datetime" name="start_datetime" required>
+              </div>
+              </div>
+              <div class="col-md-6">
+            <div class="input-block">
+              <label  for="end_datetime" class="input-label">End Date and Time:</label>
+              <input type="datetime-local" value="<?php echo $result->end_date; ?>" id="end_datetime" name="end_datetime">
+            </div>
+            </div>
+          </div>
+        
+        <div class="input-block">
+        <label  for="event_description" class="input-label">Description:</label>                                               
+        <textarea id="event_description"  name="event_description" rows="3" cols="50"><?php echo stripcslashes(sanitize_text_field($result->description)) ; ?></textarea>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+              <div class="input-block">               
+                <label  for="event_address_name" class="input-label">Location:</label>
+                <input type="text"     value="<?php echo sanitize_text_field($result->location_name)?>" id="event_address_name" name="event_address_name" required>
+              </div>
+            </div>
+            <div class="col-md-6">
+             <div class="input-block">
+                <label  for="event_address_url" class="input-label">Location URL:</label>
+               <input type="text"  value="<?php echo $result->location_url?>"  id="event_address_url"  name="event_address_url" >
+              </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+        
+        <div class="input-block">  
+        <label  for="event_address" class="input-label">Address:</label>
+        <input type="text" name="event_address"  id="event_address" value="<?php echo sanitize_text_field($result->location_address)?>" required>
+        </div>
+        </div>
+            <div class="col-md-6">
+        <div class="input-block">
+        <label  for="event_location" class="input-label">Location:</label>
+        <input type="text" name="event_location"   id="event_location" value="<?php echo $result->location_map?>">  
+        </div>
+
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+
+        <div class="modal-buttons">        
+        <button class="input-button">Login</button>
+      </div>
+
+      </div>
+        </div>
+
+        </div>
+          
+
+      
+      
+      
+      </form>
+     </div>
+
+     <div class="modal-right">
+      <img src="https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dfd2ec5a01006fd8c4d7592a381d3776&auto=format&fit=crop&w=1000&q=80" alt="">
+    </div>
+
+
+     
+     <button class="icon-button close-button">
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+     <path d="M 25 3 C 12.86158 3 3 12.86158 3 25 C 3 37.13842 12.86158 47 25 47 C 37.13842 47 47 37.13842 47 25 C 47 12.86158 37.13842 3 25 3 z M 25 5 C 36.05754 5 45 13.94246 45 25 C 45 36.05754 36.05754 45 25 45 C 13.94246 45 5 36.05754 5 25 C 5 13.94246 13.94246 5 25 5 z M 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.980469 15.990234 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 z"></path>
+    </svg>
+      </button>
+      
+   </div>
+   <button class="modal-button">Edit Event</button>
+   
+</div>
+
+
+
+
+
 
 <!-- Bootstrap JS and jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -482,7 +814,38 @@ $end_time = $endDateTime->format('h:i A'); // Time in 'HH:MM:SS' format
 <script>
 
 
+const body = document.querySelector("body");
+const modal = document.querySelector(".modal1");
+const modalButton = document.querySelector(".modal-button");
+const closeButton = document.querySelector(".close-button");
+const scrollDown = document.querySelector(".scroll-down");
+let isOpened = false;
 
+const openModal = () => {
+  modal.classList.add("is-open");
+  body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.classList.remove("is-open");
+  body.style.overflow = "initial";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight / 3 && !isOpened) {
+    isOpened = true;
+    scrollDown.style.display = "none";
+    openModal();
+  }
+});
+
+modalButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+
+document.onkeydown = evt => {
+  evt = evt || window.event;
+  evt.keyCode === 27 ? closeModal() : false;
+};
 
 
 
