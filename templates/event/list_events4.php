bf20fdb1d46a8ae3d35d21e791ea2c02ac9248b2
Nader Mitry <johndoe@example.com>
2024-05-14 15:02:54 +0200

    sssss
    Signed-off-by: Nader Mitry <johndoe@example.com>

--- templates/event/list_events4.php
+++ templates/event/list_events4.php
@@ -8,18 +8,33 @@
         <?php foreach ($results as $result) : $gifts = $this->gifts($result->id); ?> 
 
             <article class="postcard light blue">
+              
                 <a class="postcard__img_link" href="<?php echo home_url('/event').'/?eid='.$result->id ?>">
                     <img class="postcard__img" src="<?php echo plugin_dir_url( dirname( __FILE__, 2 ) ) .'assets\images\events\\'. $result->event_image;  ?>" alt="Image Title" />
                 </a>
                 <div class="postcard__text t-dark">
-                    <h1 class="postcard__title blue"><a href="<?php echo home_url('/event').'/?eid='.$result->id ?>"><?php echo stripcslashes($result->title) ?></a></h1>
+
+
+
+
+ 
+
+
+                    <h1 class="postcard__title blue"><a href="<?php echo home_url('/event').'/?eid='.$result->id ?>"><?php echo stripcslashes(sanitize_text_field($result->title)) ?></a></h1>
+                    <div class="code">
+  <?php if ($result->is_active == 1) :?>
+    <span class="badge-active">Active</span>   
+  <?php else : ?>  
+    <span class="badge-inactive">In-active</span>   
+  <?php endif ?>         
+</div> 
                     <div class="postcard__subtitle small">
                         <time datetime="2020-05-25 12:00:00">
                             <i class="fas fa-calendar-alt mr-2"></i> <?php echo $this->formatDate($result->start_date);?>
                         </time>
                     </div>
                     <div class="postcard__bar"></div>
-                    <div class="postcard__preview-txt"><?php echo $result->description ?></div>
+                    <div class="postcard__preview-txt"><?php echo stripcslashes(sanitize_text_field($result->description)) ?></div>
                     <ul class="postcard__tagbox">
                        <!-- <li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
                         <li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>-->
